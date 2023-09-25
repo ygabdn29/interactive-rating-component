77e528fe9c4f7d2178aad67b554c34b41dfc3ddc
Yoga <yogabudiman07@gmail.com>
2023-09-25 18:22:34 +0700

    Rating state start done

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 console.log("test");
 const rating = document.querySelector(".rating-number");
-
+const ratingIndex = document.querySelectorAll(".rating-index");
 rating.addEventListener("click", function (e) {
+  ratingIndex.forEach((rI) => rI.classList.remove("rating-active"));
   if (e.target.classList.contains("rating-index")) {
     const rate = e.target.dataset.value;
-    e.target.
+    e.target.classList.toggle("rating-active");
   }
 });
