803af5ad8bd88c6ed766e9623d904c3126bc7b4e
Yoga <yogabudiman07@gmail.com>
2023-09-25 16:51:39 +0700

    Minor

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const rating = document.querySelector(".rating-number");
 rating.addEventListener("click", function (e) {
   if (e.target.classList.contains("rating-index")) {
     const rate = e.target.dataset.value;
-    console.log(rate);
+    e.target.
   }
 });
